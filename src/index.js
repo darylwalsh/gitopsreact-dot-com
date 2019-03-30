c5571f26ee0c54c2c0129c6381a1c73c0e75ecf0
Daryl <bfc@freestabby.com>
2019-03-30 05:25:58 -0400

    feat: initial setup of package.json and linting

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import '@babel/polyfill'
+import http from 'http'
+function requestHandler(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello, World!')
+}
+const server = http.createServer(requestHandler)
+server.listen(8080) // Note that there is a newline below this line
