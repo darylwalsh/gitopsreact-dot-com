edd175815fce4f0410d7e26f7e9544e52957a9ac
dmw <darylwalsh@gmail.com>
2019-03-31 09:23:03 -0400

    Migrate API to Express

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,17 +1,38 @@
+import assert from 'assert'
 import superagent from 'superagent'
 import { When, Then } from 'cucumber'
-import assert from 'assert'
+import { getValidPayload, convertStringToArray } from './utils'
 
-When('the client creates a POST request to /users', function() {
-  this.request = superagent(
-    'POST',
-    `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}/users`
-  )
+When(
+  /^the client creates a (GET|POST|PATCH|PUT|DELETE|OPTIONS|HEAD) request to ([/\w-:.]+)$/,
+  function(method, path) {
+    this.request = superagent(
+      method,
+      `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}${path}`
+    )
+  }
+)
+
+When(/^attaches a generic (.+) payload$/, function(payloadType) {
+  switch (payloadType) {
+    case 'malformed':
+      this.request
+        .send('{"email": "bfc@freestabby.com", name: }')
+        .set('Content-Type', 'application/json')
+      break
+    case 'non-JSON':
+      this.request
+        .send(
+          '<?xml version="1.0" encoding="UTF-8" ?><email>bfc@freestabby.com</email>'
+        )
+        .set('Content-Type', 'text/xml')
+      break
+    case 'empty':
+    default:
+  }
 })
-When('attaches a generic empty payload', function() {
-  return undefined
-})
-When('sends the request', function(callback) {
+
+When(/^sends the request$/, function(callback) {
   this.request
     .then(response => {
       this.response = response.res
@@ -22,10 +43,14 @@ When('sends the request', function(callback) {
       callback()
     })
 })
-Then('our API should respond with a 400 HTTP status code', function() {
-  assert.equal(this.response.statusCode, 400)
+
+Then(/^our API should respond with a ([1-5]\d{2}) HTTP status code$/, function(
+  statusCode
+) {
+  assert.equal(this.response.statusCode, statusCode)
 })
-Then('the payload of the response should be a JSON object', function() {
+
+Then(/^the payload of the response should be a JSON object$/, function() {
   // Check Content-Type header
   const contentType =
     this.response.headers['Content-Type'] ||
@@ -33,6 +58,7 @@ Then('the payload of the response should be a JSON object', function() {
   if (!contentType || !contentType.includes('application/json')) {
     throw new Error('Response not of Content-Type application/json')
   }
+
   // Check it is valid JSON
   try {
     this.responsePayload = JSON.parse(this.response.text)
@@ -41,9 +67,60 @@ Then('the payload of the response should be a JSON object', function() {
   }
 })
 
-Then(
-  'contains a message property which says "Payload should not be empty"',
-  function() {
-    assert.equal(this.responsePayload.message, 'Payload should not be empty')
+Then(/^contains a message property which says (?:"|')(.*)(?:"|')$/, function(
+  message
+) {
+  assert.equal(this.responsePayload.message, message)
+})
+
+When(/^without a (?:"|')([\w-]+)(?:"|') header set$/, function(headerName) {
+  this.request.unset(headerName)
+})
+
+When(
+  /^attaches an? (.+) payload which is missing the ([a-zA-Z0-9, ]+) fields?$/,
+  function(payloadType, missingFields) {
+    this.requestPayload = getValidPayload(payloadType)
+    const fieldsToDelete = convertStringToArray(missingFields)
+    fieldsToDelete.forEach(field => delete this.requestPayload[field])
+    this.request
+      .send(JSON.stringify(this.requestPayload))
+      .set('Content-Type', 'application/json')
+  }
+)
+
+When(
+  /^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are)(\s+not)? a ([a-zA-Z]+)$/,
+  function(payloadType, fields, invert, type) {
+    this.requestPayload = getValidPayload(payloadType)
+    const typeKey = type.toLowerCase()
+    const invertKey = invert ? 'not' : 'is'
+    const sampleValues = {
+      string: {
+        is: 'string',
+        not: 10
+      }
+    }
+    const fieldsToModify = convertStringToArray(fields)
+    fieldsToModify.forEach(field => {
+      this.requestPayload[field] = sampleValues[typeKey][invertKey]
+    })
+    this.request
+      .send(JSON.stringify(this.requestPayload))
+      .set('Content-Type', 'application/json')
+  }
+)
+
+When(
+  /^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are) exactly (.+)$/,
+  function(payloadType, fields, value) {
+    this.requestPayload = getValidPayload(payloadType)
+    const fieldsToModify = convertStringToArray(fields)
+    fieldsToModify.forEach(field => {
+      this.requestPayload[field] = value
+    })
+    this.request
+      .send(JSON.stringify(this.requestPayload))
+      .set('Content-Type', 'application/json')
   }
 )
--- src/index.js
+++ src/index.js
@@ -1,19 +1,98 @@
 import '@babel/polyfill'
-import http from 'http'
+import express from 'express'
+import bodyParser from 'body-parser'
 
-function requestHandler(req, res) {
-  if (req.method === 'POST' && req.url === '/users') {
-    res.writeHead(400, { 'Content-Type': 'application/json' })
-    res.end(
-      JSON.stringify({
-        message: 'Payload should not be empty'
-      })
-    )
-    return
+const app = express()
+
+function checkEmptyPayload(req, res, next) {
+  if (
+    ['POST', 'PATCH', 'PUT'].includes(req.method) &&
+    req.headers['content-length'] === '0'
+  ) {
+    res.status(400)
+    res.set('Content-Type', 'application/json')
+    res.json({
+      message: 'Payload should not be empty'
+    })
   }
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello, World!')
+  next()
 }
 
-const server = http.createServer(requestHandler)
-server.listen(8080) // Note that there is a newline below this line
+function checkContentTypeIsSet(req, res, next) {
+  if (req.headers['content-length'] !== '0' && !req.headers['content-type']) {
+    res.status(400)
+    res.set('Content-Type', 'application/json')
+    res.json({
+      message:
+        'The "Content-Type" header must be set for requests with a non-empty payload'
+    })
+  }
+  next()
+}
+
+function checkContentTypeIsJson(req, res, next) {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(415)
+    res.set('Content-Type', 'application/json')
+    res.json({
+      message: 'The "Content-Type" header must always be "application/json"'
+    })
+  }
+  next()
+}
+
+app.use(checkEmptyPayload)
+app.use(checkContentTypeIsSet)
+app.use(checkContentTypeIsJson)
+app.use(bodyParser.json({ limit: 1e6 }))
+
+app.post('/users', (req, res, next) => {
+  if (
+    !Object.prototype.hasOwnProperty.call(req.body, 'email') ||
+    !Object.prototype.hasOwnProperty.call(req.body, 'password')
+  ) {
+    res.status(400)
+    res.set('Content-Type', 'application/json')
+    res.json({
+      message: 'Payload must contain at least the email and password fields'
+    })
+  }
+  if (
+    typeof req.body.email !== 'string' ||
+    typeof req.body.password !== 'string'
+  ) {
+    res.status(400)
+    res.set('Content-Type', 'application/json')
+    res.json({
+      message: 'The email and password fields must be of type string'
+    })
+    return
+  }
+  if (!/^[\w.+]+@\w+\.\w+$/.test(req.body.email)) {
+    res.status(400)
+    res.set('Content-Type', 'application/json')
+    res.json({ message: 'The email field must be a valid email.' })
+    return
+  }
+  next()
+})
+
+app.use((err, req, res, next) => {
+  if (
+    err instanceof SyntaxError &&
+    err.status === 400 &&
+    'body' in err &&
+    err.type === 'entity.parse.failed'
+  ) {
+    res.status(400)
+    res.set('Content-Type', 'application/json')
+    res.json({ message: 'Payload should be in JSON format' })
+    return
+  }
+  next()
+})
+
+app.listen(process.env.SERVER_PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hobnob API server listening on port ${process.env.SERVER_PORT}!`)
+})
