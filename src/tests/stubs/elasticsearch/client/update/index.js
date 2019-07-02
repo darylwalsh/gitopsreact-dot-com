da7eb9b8855eea678617f25c4bc1a4eb3d1102e7
Daryl <bfc@freestabby.com>
2019-07-02 03:26:09 -0400

    fix: update: index.js

--- src/tests/stubs/elasticsearch/client/update/index.js
+++ src/tests/stubs/elasticsearch/client/update/index.js
@@ -1,23 +1,20 @@
-import { stub } from 'sinon';
-import NotFoundError from '../../errors/not-found';
+import { stub } from 'sinon'
+import NotFoundError from '../../errors/not-found'
 
-const UPDATE_RESOLVE_OBJ = {};
-const UPDATE_REJECT_NOT_FOUND_ERROR = new NotFoundError('Not Found');
-const UPDATE_REJECT_ERROR = new Error();
+const UPDATE_RESOLVE_OBJ = {}
+const UPDATE_REJECT_NOT_FOUND_ERROR = new NotFoundError('Not Found')
+const UPDATE_REJECT_ERROR = new Error()
 
 const generate = {
   success() {
-    return stub().returns(Promise.resolve(UPDATE_RESOLVE_OBJ));
+    return stub().returns(Promise.resolve(UPDATE_RESOLVE_OBJ))
   },
   notFound() {
-    return stub().returns(Promise.reject(UPDATE_REJECT_NOT_FOUND_ERROR));
+    return stub().returns(Promise.reject(UPDATE_REJECT_NOT_FOUND_ERROR))
   },
   failure() {
-    return stub().returns(Promise.reject(UPDATE_REJECT_ERROR));
+    return stub().returns(Promise.reject(UPDATE_REJECT_ERROR))
   },
-};
+}
 
-export {
-  generate as default,
-  UPDATE_RESOLVE_OBJ,
-};
+export { generate as default, UPDATE_RESOLVE_OBJ }
